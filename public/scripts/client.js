78a62d49f86d036bcebdf66ff49536d695a7d151
Amy Bartels <akunrau@gmail.com>
2020-04-22 23:01:27 +0000

    changed the form submission process from the default to Ajax. Removed the hardcoded <article> from index.html. Adjusted tweets.css slightly with a margin to space out the tweets from one another.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,8 @@
  */
 
 $(document).ready(function() {
+  
+  // HARD CODED DATA FOR TESTING
   const tweetData =   {
     "user": {
       "name": "Newton",
@@ -40,58 +42,94 @@ $(document).ready(function() {
       },
       "created_at": 1461113959088
     }
-  ]
+  ];
 
-  
+  // HELPER FUNCTIONS TO CREATE A TWEET AND RENDER ON PAGE
+ 
+  // loops through all tweets from /tweets route and sends each tweet into createTweetElement()  before rending in the browser.
+  const renderTweets = function(tweets) {
+    const $tweetsContainer = $('#tweets-container');
+    for (const tweet of tweets) {
+      console.log(tweet)
+      $tweetsContainer.append(createTweetElement(tweet));
+    }
+  };
+
   
   const createTweetElement = function(tweetObj) {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    const tweetUser = tweetObj.user
+    const tweetContent = tweetObj.content 
+    const tweetCreated = tweetObj.created_at
+    const today = Date.now();
+    const daysDifference = Math.round((today - tweetCreated) / (1000 * 3600 * 24))
     
+    const $tweet = $(`
+      <article class="tweet-body hover-tweet">
+        <header class="tweet-header">
+          <div class="tweet-left">
+            <img class="tweet-img" src="${tweetUser.avatars}">
+            <p class="tweet-name">${tweetUser.name}</p>
+          </div>
+          <p class="tweet-right hover-tweet">${tweetUser.handle}</p>
+        </header>
+        <section class="tweet-content">
+          <p class="tweet">${tweetContent.text}</p>
+        </section>
+        <footer class="tweet-footer">
+          <p class="tweet-date">${daysDifference} days ago</p>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
     return $tweet;
-  }
+  };
+
+  $.getJSON('/tweets')
+  .then(renderTweets)
+
+
+  $('.tweet-form').submit(function(event) {
+    event.preventDefault();
+    const formData = $(this).serialize();
+    $.post('/tweets', formData)
+
+    .then(() => {
+      $(this)[0].reset();
+      $.getJSON('/tweets')
+      .then(renderTweets)
+    })
+
+    .catch(err => console.log('error >', err))
+  })
   
-  const $tweet = createTweetElement(tweetData);
-  
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
   
 
-  const renderTweets = function(tweetArr) {
-    $.getJSON('/tweets')
-      .then((tweets) => {
-        // console.log("tweets >>", tweets);
-        const $tweetsContainer = $('#tweets-container');
-        for (const tweet of tweets) {
-          console.log("tweet >!>", tweet);
-          const tweetHTML = `
-          <article class="tweet-body hover-tweet">
-            <header class="tweet-header">
-              <div class="tweet-left">
-                <img class="tweet-img" src="${tweet.user.avatars}">
-                <p class="tweet-name">${tweet.user.name}</p>
-              </div>
-                <p class="tweet-right hover-tweet">${tweet.user.handle}</p>
-            </header>
-            <section class="tweet-content">
-              <p class="tweet">${tweet.content.text}</p>
-            </section>
-            <footer class="tweet-footer">
-              <p class="tweet-date">10 days ago</p>
-              <div class="tweet-icons">
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-            </footer>
-          </article>
-          `
-          $tweetsContainer.append(tweetHTML);
-        }
-      })
-  }
 
-  renderTweets(data)
+
+  
 
 // end of document ready function
 });
 
+// from lecture....:
+
+// $('form').submit(function(event){
+//   // prevent the default behaviour of html form
+//   event.preventDefault();
+//   const self = this;
+//   // extract data from `this` - the form
+//   const formData = $(this).serialize(); // urlencoded serialization format
+//   // make ajax post with extracted data
+//   $.post('/api/posts', formData)
+//   // if sucess - render updated posts
+//   .then(() => {
+//     // $('form') but that is slightly less performant
+//     $(self)[0].reset();
+//     loadPosts()
+//   })
+//   // else - do nothing; console.error
+//   .catch(err => console.log('err :>> ', err))
