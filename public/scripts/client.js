e8a79035a107897f9456201d6113374d8dc3bc14
Amy Bartels <akunrau@gmail.com>
2020-04-22 20:57:47 +0000

    added createTweetElement and renderTweet functions in client.js. Still only with hardcoded test data.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,94 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const tweetData =   {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  
+  
+  const createTweetElement = function(tweetObj) {
+    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    
+    return $tweet;
+  }
+  
+  const $tweet = createTweetElement(tweetData);
+  
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
+  
+
+  const renderTweets = function(tweetArr) {
+    $.getJSON('/tweets')
+      .then((tweets) => {
+        // console.log("tweets >>", tweets);
+        const $tweetsContainer = $('#tweets-container');
+        for (const tweet of tweets) {
+          console.log("tweet >!>", tweet);
+          const tweetHTML = `
+          <article class="tweet-body hover-tweet">
+            <header class="tweet-header">
+              <div class="tweet-left">
+                <img class="tweet-img" src="${tweet.user.avatars}">
+                <p class="tweet-name">${tweet.user.name}</p>
+              </div>
+                <p class="tweet-right hover-tweet">${tweet.user.handle}</p>
+            </header>
+            <section class="tweet-content">
+              <p class="tweet">${tweet.content.text}</p>
+            </section>
+            <footer class="tweet-footer">
+              <p class="tweet-date">10 days ago</p>
+              <div class="tweet-icons">
+                <i class="fas fa-flag"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="fas fa-heart"></i>
+              </div>
+            </footer>
+          </article>
+          `
+          $tweetsContainer.append(tweetHTML);
+        }
+      })
+  }
+
+  renderTweets(data)
+
+// end of document ready function
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,22 @@
 
-
 $(document).ready(function() {
-  console.log('jQuery ready')
-});
-
-
-
-const $textField = $('.tweet-field');
-
-const $counter = $('.counter')
-
-$textField.keyup(function () {
-  const counterNum = 140
-  let $tweetLength = $(this).val().length;
+  // console.log('jQuery ready')
+  const $textField = $('.tweet-field');
+  const $counter = $('.counter')
   
-  let newLength = counterNum - $tweetLength
-  $counter.val(`${newLength}`)
-  
-  if (newLength < 0 && !$counter.hasClass("counter-over")){
-    console.log("over")
-    $counter.addClass("counter-over")
-  } else if (newLength >= 0 && $counter.hasClass("counter-over")) {
-    $counter.removeClass("counter-over")
-  }
+  $textField.keyup(function () {
+    const counterNum = 140
+    let $tweetLength = $(this).val().length;
+    
+    let newLength = counterNum - $tweetLength
+    $counter.val(`${newLength}`)
+    
+    if (newLength < 0 && !$counter.hasClass("counter-over")){
+      console.log("over")
+      $counter.addClass("counter-over")
+    } else if (newLength >= 0 && $counter.hasClass("counter-over")) {
+      $counter.removeClass("counter-over")
+    }
+  });
+
 });
