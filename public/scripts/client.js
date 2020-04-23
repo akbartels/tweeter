a9fca7bf88f553753a34a2ae52a95f5906a53da4
Amy Bartels <akunrau@gmail.com>
2020-04-23 02:51:27 +0000

    added conditional in client.js to alert the user if they submitted with no characters or too many characters. Also added in an escape function to ensure files could not be altered via the input.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,51 +7,58 @@
 $(document).ready(function() {
   
   // HARD CODED DATA FOR TESTING
-  const tweetData =   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
+  // const tweetData =   {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //   "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //   "created_at": 1461116232227
+  // }
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   // HELPER FUNCTIONS TO CREATE A TWEET AND RENDER ON PAGE
  
+  // ensures input cannot be used to alter any files
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   // loops through all tweets from /tweets route and sends each tweet into createTweetElement()  before rending in the browser.
   const renderTweets = function(tweets) {
     const $tweetsContainer = $('#tweets-container');
     for (const tweet of tweets) {
       console.log(tweet)
-      $tweetsContainer.append(createTweetElement(tweet));
+      $tweetsContainer.prepend(createTweetElement(tweet));
     }
   };
 
@@ -67,16 +74,16 @@ $(document).ready(function() {
       <article class="tweet-body hover-tweet">
         <header class="tweet-header">
           <div class="tweet-left">
-            <img class="tweet-img" src="${tweetUser.avatars}">
-            <p class="tweet-name">${tweetUser.name}</p>
+            <img class="tweet-img" src="${escape(tweetUser.avatars)}">
+            <p class="tweet-name">${escape(tweetUser.name)}</p>
           </div>
-          <p class="tweet-right hover-tweet">${tweetUser.handle}</p>
+          <p class="tweet-right hover-tweet">${escape(tweetUser.handle)}</p>
         </header>
         <section class="tweet-content">
-          <p class="tweet">${tweetContent.text}</p>
+          <p class="tweet">${escape(tweetContent.text)}</p>
         </section>
         <footer class="tweet-footer">
-          <p class="tweet-date">${daysDifference} days ago</p>
+          <p class="tweet-date">${escape(daysDifference)} days ago</p>
           <div class="tweet-icons">
             <i class="fas fa-flag"></i>
             <i class="fas fa-retweet"></i>
@@ -88,29 +95,49 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  $.getJSON('/tweets')
-  .then(renderTweets)
+ // using jQuery AJAX GET request
+  const loadTweets = function() {
+    $.getJSON('/tweets')
+    .then(renderTweets)
+  }
+ 
 
-
+// jQuery AJAX POST request
   $('.tweet-form').submit(function(event) {
     event.preventDefault();
     const formData = $(this).serialize();
-    $.post('/tweets', formData)
 
+    // const $textField = $('.tweet-field');
+    const $counter = Number($('.counter')[0].innerText);
+    
+    if ($counter === 140) {
+      alert("😅 Oops! You didn't add your message! \n\nWhat are you humming about?");
+      return;
+    } else if ($counter < 0) {
+      alert("😩 Oops! You went over the 140 character limit. \n\nRemove some text and try again.");
+      return;
+    } 
+
+    $.post('/tweets', formData)
     .then(() => {
       $(this)[0].reset();
-      $.getJSON('/tweets')
-      .then(renderTweets)
+      loadTweets();
     })
+    
+    
+    
+    
 
-    .catch(err => console.log('error >', err))
+    // .catch(err => console.log('error >', err))
   })
   
-  
+
+
+loadTweets();
 
 
 
-  
+
 
 // end of document ready function
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,20 +3,50 @@ $(document).ready(function() {
   // console.log('jQuery ready')
   const $textField = $('.tweet-field');
   const $counter = $('.counter')
+
+$textField.keyup(function () {
+    
+  const counterNum = 140
+  let $tweetLength = $(this).val().length;
   
-  $textField.keyup(function () {
-    const counterNum = 140
-    let $tweetLength = $(this).val().length;
-    
-    let newLength = counterNum - $tweetLength
-    $counter.val(`${newLength}`)
-    
-    if (newLength < 0 && !$counter.hasClass("counter-over")){
-      console.log("over")
-      $counter.addClass("counter-over")
-    } else if (newLength >= 0 && $counter.hasClass("counter-over")) {
-      $counter.removeClass("counter-over")
-    }
-  });
+  let newLength = counterNum - $tweetLength
+  $counter.val(`${newLength}`)
+  
+  if (newLength < 0 && !$counter.hasClass("counter-over")){
+    console.log("over")
+    $counter.addClass("counter-over")
+  } else if (newLength >= 0 && $counter.hasClass("counter-over")) {
+    $counter.removeClass("counter-over")
+  }
+});
+
+
+
+  
 
 });
+
+
+
+
+
+// $textField.keyup(function (e) {
+//   console.log($(this ))
+// const output = $(this).siblings().last();
+// let counter = e.target.value.length
+
+// // let $tweetLength = $(this).val().length;
+
+// // let newLength = counterNum - $tweetLength
+// // $counter.val(`${newLength}`)
+
+// if (counter > 140){
+  
+//   output.addClass("over")
+// } else {
+//   output.removeClass("over")
+// }
+
+// output.text(140 - counter)
+
+// });
